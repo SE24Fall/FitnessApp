fcf7927ebd0cb2cb2ade08bea37db98c718000c1
ashwinchelsea14 <ashwinchelsea14@gmail.com>
2024-11-26 11:35:49 -0500

    added track_weight feature

--- application.py
+++ application.py
@@ -505,6 +505,51 @@ def calories():
 #     # return render_template('user_profile.html', status=True, form=form) #
 
 
+@app.route("/track_weight", methods=['GET', 'POST'])
+def track_weight():
+    """
+    Track your weight and display in graph
+    """
+    now = datetime.now().strftime('%Y-%m-%d')
+
+    if session.get('email'):
+        email = session.get('email')
+        user_data = mongo.db.profile.find_one({'email': email})
+        if not user_data:
+            flash('User profile not found.', 'danger')
+            return redirect(url_for('user_profile'))
+        # target_weight = float(user_data['target_weight'])
+        # user_data_hist = list(mongo.db.profile.find({'email': email}))
+
+        # for entry in user_data_hist:
+        #     entry['date'] = datetime.strptime(entry['date'], '%Y-%m-%d').date()
+
+        # sorted_user_data_hist = sorted(user_data_hist, key=lambda x: x['date'])
+        # # Extracting data for the graph
+        # dates = [entry['date'] for entry in sorted_user_data_hist]
+        # weights = [float(entry['weight']) for entry in sorted_user_data_hist]
+
+        # # Plotting Graph
+        # fig = px.line(x=dates, y=weights, labels={
+        #               'x': 'Date', 'y': 'Weight'}, title='Progress', markers=True, line_shape='spline')
+        # fig.add_trace(go.Scatter(x=dates, y=[target_weight] * len(dates),
+        #                          mode='lines',
+        #                          line=dict(color='green', width=1, dash='dot'),
+        #                          name='Target Weight'))
+        # fig.update_yaxes(
+        #     range=[min(min(weights), target_weight) - 5, max(max(weights), target_weight) + 5])
+        # fig.update_xaxes(
+        #     range=[min(dates), datetime.strptime(now, '%Y-%m-%d').date()])
+        # # Converting to HTML
+        # graph_html = fig.to_html(full_html=False)
+
+        # last_10_entries = sorted_user_data_hist[-10:]
+
+        return render_template('track_weight.html', status=True, user_data=user_data, graph_html=graph_html, last_10_entries=last_10_entries)
+    else:
+        return redirect(url_for('login'))
+
+
 @app.route("/user_profile", methods=['GET', 'POST'])
 def user_profile():
     """
